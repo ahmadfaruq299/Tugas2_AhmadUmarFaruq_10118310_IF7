38a10c64e25cb72754bae384b6604971685b0c07
Ahmad Faruq <61138778+ahmadfaruq299@users.noreply.github.com>
2020-04-09 02:06:17 +0700

    Tugas

---
+++ Program_Latihan_3.1/src/program_latihan_3/pkg1/Program_Latihan_31.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program_latihan_3.pkg1;
+
+/**
+ *
+ * @author AhmadFaruq
+ */
+public class Program_Latihan_31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         NewJFrame frm = new NewJFrame();
+            frm.setVisible(true);
+    }
+    
+}
